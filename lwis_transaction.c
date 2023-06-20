9736d3990fdb27edfd7382466b342ac497dcd822
Meghana Barkalle <mbarkalle@google.com>
2023-06-20 22:15:56 +0000

    LWIS: Fix race condition while disabling device
    This change will delete the transaction in the
    processing thread itself if the device is in
    a flushing state. That way if the bus manager
    worker is scheduled by a third device, then
    the disabling device's queue will not have any
    stale references.
    
    Bug: 287934746
    Test: GCA-Smoke test, CTS tests
    Signed-off-by: Meghana Barkalle <mbarkalle@google.com>
    (cherry picked from https://partner-android-review.googlesource.com/q/commit:e34b351780fb12c945efd60c57375b9f6fc77b70)
    Merged-In: Ibdcc9c1a670b080fea64fe5539aae06a7c5d8453
    Change-Id: Ibdcc9c1a670b080fea64fe5539aae06a7c5d8453

--- lwis_transaction.c
+++ lwis_transaction.c
@@ -492,6 +492,11 @@ void lwis_process_transactions_in_queue(struct lwis_client *client)
 	spin_lock_irqsave(&client->transaction_lock, flags);
 	list_for_each_safe (it_tran, it_tran_tmp, &client->transaction_process_queue) {
 		if (!client->is_enabled) {
+			/*
+			 * If client is not enabled, then we just need to requeue
+			 * the transaction until the client is enabled. This will
+			 * ensure that we don't loose the submitted transactions.
+			*/
 			if (lwis_transaction_debug) {
 				dev_info(client->lwis_dev->dev,
 					 "Client is not ready to process transactions");
@@ -544,6 +549,8 @@ void lwis_process_transactions_in_queue(struct lwis_client *client)
 				/*
 				 * Queue the remaining transaction again on the transaction worker/bus maanger worker
 				 * to be processed again later if the client is not flushing
+				 * If the client is flushing, cancel the remaining transaction
+				 * and delete from the process queue node.
 				 */
 				spin_lock_irqsave(&client->flush_lock, flags);
 				if (client->flush_state == NOT_FLUSHING) {
@@ -566,6 +573,10 @@ void lwis_process_transactions_in_queue(struct lwis_client *client)
 								client->lwis_dev->dev,
 								"Client is flushing, aborting the remaining transaction");
 					}
+					list_del(&transaction->process_queue_node);
+					cancel_transaction(client->lwis_dev, &transaction,
+					   transaction->resp->error_code, &pending_events,
+					   &pending_fences, false);
 				}
 				spin_unlock_irqrestore(&client->flush_lock, flags);
 				break;
